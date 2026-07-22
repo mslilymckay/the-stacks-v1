3c2c4ce071d6f6f8fb6c59f6ebe510d39e99d6ef
mslilymckay <lily.morin423@gmail.com>
2026-07-22 14:05:47 -0500

    Align style and functionality with Gold v1

--- app.js
+++ app.js
@@ -2498,6 +2498,7 @@ function getPlaceholderCoverUrl(book) {
   const author = getField(book, 'author') || 'No Author';
   return getGenericPlaceholderCoverUrl(title, author);
 }
+window.getPlaceholderCoverUrl = getPlaceholderCoverUrl;
 
 function getCoverUrl(isbn, book = null) {
   if (!isbn || isbn === 'N/A') {
